b753f147ec3309d25203526a32986c3364ca3bde
AniaMiller <113344473+AniaMiller@users.noreply.github.com>
2023-09-12 21:19:01 +0000

    Deploying to gh-pages from @ AniaMiller/goit-js-hw-09@0d99c2695f2ef1777d09ff9e88a9011ad8c22006 🚀

---
+++ 01-color-switcher.5d79a2d7.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=null;t.addEventListener("click",(function(){a=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),2e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.5d79a2d7.js.map
